7b2bf6a1ed8f778d3ab4d57bce35dcebfc135d4f
Julian Hoever <julianhoever@posteo.de>
2024-07-09 17:28:03 +0200

    only support uint8 dtype

--- src/ml_dev/tflite/execute_tflite_model.py
+++ src/ml_dev/tflite/execute_tflite_model.py
@@ -4,18 +4,17 @@ import numpy as np
 import tensorflow as tf
 
 
-def execute_tflite_model(tflite_file: Path, samples: np.ndarray) -> np.ndarray:
+def execute_uint8_tflite_model(tflite_file: Path, samples: np.ndarray) -> np.ndarray:
     interpreter = tf.lite.Interpreter(model_path=str(tflite_file))
     interpreter.allocate_tensors()
 
     input_details = interpreter.get_input_details()[0]
     output_details = interpreter.get_output_details()[0]
 
-    predictions = np.zeros((len(samples),), dtype=np.int32)
+    predictions = np.zeros(len(samples), dtype=np.int32)
     for i, sample in enumerate(samples):
-        if input_details["dtype"] == np.uint8:
-            input_scale, input_zero_point = input_details["quantization"]
-            sample = sample / input_scale + input_zero_point
+        input_scale, input_zero_point = input_details["quantization"]
+        sample = sample / input_scale + input_zero_point
 
         sample = np.expand_dims(sample, axis=0).astype(input_details["dtype"])
         interpreter.set_tensor(input_details["index"], sample)
