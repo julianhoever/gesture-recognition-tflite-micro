4b44676e73789390bf660ac763da147f1b4e10f2
Julian Hoever <julianhoever@posteo.de>
2024-07-10 08:56:26 +0200

    fix wrong accuracy metric

--- src/ml_dev/scripts/run_training.py
+++ src/ml_dev/scripts/run_training.py
@@ -1,4 +1,5 @@
 from functools import partial
+from pathlib import Path
 
 import keras
 import tensorflow as tf
@@ -13,28 +14,26 @@ from ml_dev.environment import DATA_ROOT, OUTPUTS_DIR, KERAS_MODEL_FILE
 def main() -> None:
     OUTPUTS_DIR.mkdir(exist_ok=True)
 
-    window_size = 125
-
-    (x_train, y_train), (x_val, y_val) = _load_data(window_size)
-    model = gesture_cnn_model((None, window_size, 3))
+    (x_train, y_train), (x_val, y_val) = _load_data()
+    model = gesture_cnn_model((None, 125, 3))
     model.summary()
 
     model.compile(
-        optimizer=keras.optimizers.Adam(learning_rate=5e-3),
+        optimizer=keras.optimizers.Adam(learning_rate=1e-3),
         loss=keras.losses.CategoricalCrossentropy(),
-        metrics=[keras.metrics.Accuracy()],
+        metrics=[keras.metrics.CategoricalAccuracy()],
     )
     history = model.fit(
         x=x_train,
         y=y_train,
         validation_data=(x_val, y_val),
-        batch_size=512,
-        epochs=100,
+        batch_size=64,
+        epochs=20,
         shuffle=True,
         callbacks=[
             keras.callbacks.ModelCheckpoint(
                 filepath=KERAS_MODEL_FILE,
-                monitor="val_accuracy",
+                monitor="val_categorical_accuracy",
                 save_best_only=True,
             )
         ],
@@ -42,16 +41,12 @@ def main() -> None:
     model.load_weights(KERAS_MODEL_FILE)
     model.evaluate(x=x_val, y=y_val)
 
-    plt.plot(history.history["loss"])
-    plt.plot(history.history["val_loss"])
-    plt.show()
+    _plot_train_history(history)
 
 
-def _load_data(
-    window_size: int,
-) -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
+def _load_data() -> tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]]:
     load_data = partial(
-        load_gesture_data, data_root=DATA_ROOT, window_size=window_size, stride=1
+        load_gesture_data, data_root=DATA_ROOT, window_size=125, stride=1
     )
     x_train, y_train = load_data(training=True)
     x_val, y_val = load_data(training=False)
@@ -60,6 +55,13 @@ def _load_data(
     x_val = normalize(x_val)
 
     return (x_train, y_train), (x_val, y_val)
+
+
+def _plot_train_history(history: keras.callbacks.History) -> None:
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.plot(history.history["loss"])
+    ax.plot(history.history["val_loss"])
+    fig.savefig(OUTPUTS_DIR / "train_history.png")
 
 
 if __name__ == "__main__":
