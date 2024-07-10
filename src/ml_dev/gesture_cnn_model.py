e12176d4dace49ef67f8b6846538d8378fe1dbdf
Julian Hoever <julianhoever@posteo.de>
2024-07-10 08:55:52 +0200

    simplify model

--- src/ml_dev/gesture_cnn_model.py
+++ src/ml_dev/gesture_cnn_model.py
@@ -2,22 +2,13 @@ import keras
 
 
 def gesture_cnn_model(input_shape: tuple[int | None, ...]) -> keras.Sequential:
-    def conv_block(filters: int, kernel_size: int) -> keras.Sequential:
-        return keras.Sequential(
-            [
-                keras.layers.Conv1D(filters=filters, kernel_size=kernel_size),
-                keras.layers.ReLU(),
-                keras.layers.BatchNormalization(),
-            ]
-        )
-
     model = keras.Sequential(
         [
-            keras.layers.AvgPool1D(pool_size=16, strides=1),
-            conv_block(filters=16, kernel_size=4),
+            keras.layers.Conv1D(filters=32, kernel_size=4, activation="relu"),
+            keras.layers.MaxPool1D(pool_size=3),
+            keras.layers.BatchNormalization(),
+            keras.layers.Dropout(rate=0.3),
             keras.layers.Flatten(),
-            keras.layers.Dense(units=32, activation="relu"),
-            keras.layers.Dropout(rate=0.2),
             keras.layers.Dense(units=4, activation="softmax"),
         ]
     )
