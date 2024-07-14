61acfbe7789182469cf43261a1a3744d3b05b108
Julian Hoever <julianhoever@posteo.de>
2024-07-14 21:54:19 +0200

    add debug prints and make op resolver as a external dependency

--- src/hardware/app/tflite_interpreter.cpp
+++ src/hardware/app/tflite_interpreter.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
-#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+
+#include "pico/stdio.h"
+#include "tensorflow/lite/micro/micro_op_resolver.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/system_setup.h"
@@ -7,11 +9,16 @@
 
 
 TFLiteInterpreter::TFLiteInterpreter(
-    const uint8_t* modelBuffer, const uint32_t tensorArenaSize, const uint32_t inputSize, const uint32_t outputSize
+    const uint8_t* modelBuffer,
+    const tflite::MicroOpResolver* resolver,
+    const uint32_t tensorArenaSize,
+    const uint32_t inputFeatureCount,
+    const uint32_t outputFeatureCount
 ) : modelBuffer(modelBuffer),
+    resolver(resolver),
     tensorArenaSize(tensorArenaSize),
-    inputSize(inputSize),
-    outputSize(outputSize),
+    inputFeatureCount(inputFeatureCount),
+    outputFeatureCount(outputFeatureCount),
     tensorArena(new uint8_t[tensorArenaSize] { 0 }) { }
 
 
@@ -20,7 +27,7 @@ int TFLiteInterpreter::initialize() {
 
     this->model = tflite::GetModel(this->modelBuffer);
     if (this->model->version() != TFLITE_SCHEMA_VERSION) {
-        MicroPrintf(
+        printf(
             "Model provided is schema version %d not equal "
             "to supported version %d.\n",
             this->model->version(), TFLITE_SCHEMA_VERSION
@@ -28,20 +35,13 @@ int TFLiteInterpreter::initialize() {
         return -1;
     }
 
-    static tflite::MicroMutableOpResolver<1> resolver;
-    TfLiteStatus resolveStatus = resolver.AddFullyConnected();
-    if (resolveStatus != kTfLiteOk) {
-        MicroPrintf("Op resolution failed");
-        return -2;
-    }
-
     static tflite::MicroInterpreter staticInterpreter(
-        this->model, resolver, this->tensorArena, this->tensorArenaSize
+        this->model, *this->resolver, this->tensorArena, this->tensorArenaSize
     );
     this->interpreter = &staticInterpreter;
     TfLiteStatus allocateStatus = this->interpreter->AllocateTensors();
     if (allocateStatus != kTfLiteOk) {
-        MicroPrintf("AllocateTensors() failed");
+        printf("AllocateTensors() failed (Code: %d)\n", allocateStatus);
         return -3;
     }
 
@@ -53,21 +53,28 @@ int TFLiteInterpreter::initialize() {
 
 
 int TFLiteInterpreter::runInference(float* const inputBuffer, float* const outputBuffer) {
-    for (uint32_t inputIdx = 0; inputIdx < this->inputSize; inputIdx++) {
+    printf("Quantize inputs\n");
+    for (uint32_t inputIdx = 0; inputIdx < this->inputFeatureCount; inputIdx++) {
         const float x = inputBuffer[inputIdx];
         this->input->data.uint8[inputIdx] = this->quantize(x);
     }
 
+    printf("Invoke interpreter\n");
+
     TfLiteStatus invokeStatus = this->interpreter->Invoke();
     if (invokeStatus != kTfLiteOk) {
-        MicroPrintf("Invoke failed");
+        printf("Invoke failed\n");
         return -1;
     }
 
-    for (uint32_t outputIdx = 0; outputIdx < this->outputSize; outputIdx++) {
+    printf("Dequantize outputs\n");
+
+    for (uint32_t outputIdx = 0; outputIdx < this->outputFeatureCount; outputIdx++) {
         const uint8_t quant_y = this->output->data.uint8[outputIdx];
         outputBuffer[outputIdx] = dequantize(quant_y);
     }
+
+    printf("Done\n");
 
     return 0;
 }
--- src/hardware/app/tflite_interpreter.h
+++ src/hardware/app/tflite_interpreter.h
@@ -1,18 +1,29 @@
+#pragma once
+
 #include <cstdint>
+
+#include "tensorflow/lite/micro/micro_op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 
 
 class TFLiteInterpreter {
     public:
-        TFLiteInterpreter(const uint8_t* modelBuffer, const uint32_t tensorArenaSize, const uint32_t inputSize, const uint32_t outputSize);
+        TFLiteInterpreter(
+            const uint8_t* modelBuffer,
+            const tflite::MicroOpResolver* resolver,
+            const uint32_t tensorArenaSize,
+            const uint32_t inputSize,
+            const uint32_t outputSize
+        );
         int runInference(float* const inputBuffer, float* const outputBuffer);
         int initialize();
     private:
         const uint8_t* modelBuffer;
-        const uint32_t tensorArenaSize, inputSize, outputSize;
+        const uint32_t tensorArenaSize, inputFeatureCount, outputFeatureCount;
         uint8_t* tensorArena;
         const tflite::Model* model;
+        const tflite::MicroOpResolver* resolver;
         tflite::MicroInterpreter* interpreter;
         TfLiteTensor* input;
         TfLiteTensor* output;
