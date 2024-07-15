04ec0600e42c4f14c3fdeb4614facb89b7df2f71
Julian Hoever <julianhoever@posteo.de>
2024-07-15 11:47:56 +0200

    use smaller model

--- src/hardware/app/model.h
+++ src/hardware/app/model.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef MODEL_H
+#define MODEL_H
 
-extern const unsigned char outputs_model_tflite[];
-extern const unsigned int outputs_model_tflite_len;
+extern const unsigned char model_tflite[];
+extern const unsigned int model_tflite_len;
+
+#endif
