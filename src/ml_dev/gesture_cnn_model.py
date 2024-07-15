762e2cd713c8e5b6b78de1eacb816baee2668f6b
Julian Hoever <julianhoever@posteo.de>
2024-07-15 11:26:58 +0200

    reduce model size

--- src/ml_dev/gesture_cnn_model.py
+++ src/ml_dev/gesture_cnn_model.py
@@ -4,7 +4,12 @@ import keras
 def gesture_cnn_model(input_shape: tuple[int | None, ...]) -> keras.Sequential:
     model = keras.Sequential(
         [
-            keras.layers.Conv1D(filters=32, kernel_size=4, activation="relu"),
+            keras.layers.DepthwiseConv1D(kernel_size=4, activation="relu"),
+            keras.layers.Conv1D(filters=32, kernel_size=1, activation="relu"),
+            keras.layers.MaxPool1D(pool_size=3),
+            keras.layers.BatchNormalization(),
+            keras.layers.DepthwiseConv1D(kernel_size=4, activation="relu"),
+            keras.layers.Conv1D(filters=12, kernel_size=1, activation="relu"),
             keras.layers.MaxPool1D(pool_size=3),
             keras.layers.BatchNormalization(),
             keras.layers.Dropout(rate=0.3),
