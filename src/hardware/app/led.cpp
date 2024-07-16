72d3a0f138ee630d3a2d90fb181f25bcf52147cf
Julian Hoever <julianhoever@posteo.de>
2024-07-16 13:00:54 +0200

    start adding led function

---
+++ src/hardware/app/led.cpp
@@ -0,0 +1,8 @@
+#include <cstdint>
+#include "pico/stdlib.h"
+#include "led.h"
+
+
+void setRgbLed(uint8_t red, uint8_t blue, uint8_t green) {
+    
+}
---
+++ src/hardware/app/led.h
@@ -0,0 +1,8 @@
+#ifndef LED_H
+#define LED_H
+
+#include <cstdint>
+
+void setRgbLed(uint8_t red, uint8_t blue, uint8_t green);
+
+#endif
