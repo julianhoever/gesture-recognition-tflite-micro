64a5866af3d863cd35e8a8a685aa113b67bee3e9
Julian Hoever <julianhoever@posteo.de>
2024-07-17 21:57:35 +0200

    more device-oriented preprocessing

--- src/ml_dev/preprocessing.py
+++ src/ml_dev/preprocessing.py
@@ -1,18 +1,17 @@
 import tensorflow as tf
 
+ABS_MAX_DATA = 20
+ABS_MAX_DEVICE = 511
+DEVICE_SPECIFIC_SCALING = ABS_MAX_DEVICE / ABS_MAX_DATA
+
 
 def _center_channels(data: tf.Tensor) -> tf.Tensor:
     mean = tf.expand_dims(tf.reduce_mean(data, axis=-2), axis=-2)
     return data - mean
 
 
-def _rescale(data: tf.Tensor) -> tf.Tensor:
-    abs_max = tf.reduce_max(tf.abs(data), axis=(-2, -1))
-    abs_max = tf.reshape(abs_max, (-1, 1, 1))
-    return data / abs_max
-
-
 def preprocess(data: tf.Tensor) -> tf.Tensor:
-    x = _center_channels(data)
-    x = _rescale(x)
-    return x
+    data *= DEVICE_SPECIFIC_SCALING
+    data = tf.clip_by_value(data, -ABS_MAX_DEVICE, ABS_MAX_DEVICE)
+    data = _center_channels(data)
+    return data
