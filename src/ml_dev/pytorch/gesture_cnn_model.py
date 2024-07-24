2682616e75bf24a0f4b7e8fc60ead75247d2ce20
Julian Hoever <julianhoever@posteo.de>
2024-07-24 14:00:53 +0200

    use module instead of functions

--- src/ml_dev/pytorch/gesture_cnn_model.py
+++ src/ml_dev/pytorch/gesture_cnn_model.py
@@ -6,7 +6,7 @@ import torch
 class GestureCnnModel(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        conv_block = partial(_conv_block, kernel_size=4, pool_size=3)
+        conv_block = partial(_ConvBlock, kernel_size=4, pool_size=3)
         self.conv0 = conv_block(in_channels=3, out_channels=32)
         self.conv1 = conv_block(in_channels=32, out_channels=12)
         self.dropout = torch.nn.Dropout(p=0.3)
@@ -21,30 +21,29 @@ class GestureCnnModel(torch.nn.Module):
         return self.clf(x)
 
 
-def _conv_block(
-    in_channels: int, out_channels: int, kernel_size: int, pool_size: int
-) -> torch.nn.Sequential:
-    return torch.nn.Sequential(
-        _depthwise_separable_conv1d(in_channels, out_channels, kernel_size),
-        torch.nn.ReLU(),
-        torch.nn.MaxPool1d(kernel_size=pool_size),
-        torch.nn.BatchNorm1d(num_features=out_channels),
-    )
+class _ConvBlock(torch.nn.Module):
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int, pool_size: int
+    ) -> None:
+        super().__init__()
+        self.dw_conv = torch.nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            groups=in_channels,
+        )
+        self.pw_conv = torch.nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            groups=1,
+        )
+        self.relu = torch.nn.ReLU()
+        self.maxpool = torch.nn.MaxPool1d(kernel_size=pool_size)
 
-
-def _depthwise_separable_conv1d(
-    in_channels: int, out_channels: int, kernel_size: int
-) -> torch.nn.Sequential:
-    depthwise_conv = torch.nn.Conv1d(
-        in_channels=in_channels,
-        out_channels=in_channels,
-        kernel_size=kernel_size,
-        groups=in_channels,
-    )
-    pointwise_conv = torch.nn.Conv1d(
-        in_channels=in_channels,
-        out_channels=out_channels,
-        kernel_size=1,
-        groups=1,
-    )
-    return torch.nn.Sequential(depthwise_conv, pointwise_conv)
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x = self.dw_conv(inputs)
+        x = self.pw_conv(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        return x
