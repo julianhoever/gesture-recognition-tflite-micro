13311ee26f03bd5ec91657efafb5291e09215976
Julian Hoever <julianhoever@posteo.de>
2024-08-27 09:57:47 +0200

    remove pytorch

--- src/ml_dev/environment.py
+++ src/ml_dev/environment.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import torch
-
 # Paths
 
 DATA_ROOT = Path("data/gestures")
@@ -12,22 +10,6 @@ TF_MODEL_WEIGHTS_FILE = TF_OUTPUTS_DIR / "model.weights.h5"
 TF_TFLITE_MODEL_FILE = TF_OUTPUTS_DIR / "model.tflite"
 TF_CPP_ARRAY_MODEL_FILE = TF_OUTPUTS_DIR / "model.cpp"
 
-PT_OUTPUTS_DIR = OUTPUTS_DIR / "pytorch"
-PT_MODEL_WEIGHTS_FILE = PT_OUTPUTS_DIR / "model.weights.pt"
-PT_EXECUTORCH_MODEL_FILE = PT_OUTPUTS_DIR / "model.pte"
-PT_CPP_ARRAY_MODEL_FILE = PT_OUTPUTS_DIR / "model.cpp"
-
 # ML Constants
 
 SAMPLE_SHAPE = (125, 3)
-
-
-def _determine_available_device() -> str:
-    if torch.cuda.is_available():
-        return "cuda"
-    elif torch.backends.mps.is_available():
-        return "mps"
-    return "cpu"
-
-
-PT_DEVICE = _determine_available_device()
--- src/ml_dev/pytorch/training/__init__.py
+++
@@ -1,2 +0,0 @@
-from .train_model import train_model
-from .history import History
